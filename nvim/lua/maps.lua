b152a18a188a8a9d836c515f439686e4a868ed8d
BBBoy01 <1156678002@qq.com>
2022-09-21 13:44:13 +0800

    feat: tab configurations

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -18,6 +18,11 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- New tab
 keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
+-- Switch tab
+keymap.set('n', 'th', ':tabp<Return>', { silent = true })
+keymap.set('n', 'tl', ':tabn<Return>', { silent = true })
+-- Only keep current tab
+keymap.set('n', 'to', ':tabo<Return>', { silent = true })
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
