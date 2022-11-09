6a00c0282c6d5d0f9b0fef779770fd383c62af91
BBboy01 <1156678002@qq.com>
2022-11-09 14:07:22 +0800

    feat: add more efficient key maps

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -13,9 +13,6 @@ keymap.set('n', '-', '<C-x>')
 -- Delete a word backwards
 keymap.set('n', 'dw', 'vb"_d')
 
--- Select all
-keymap.set('n', '<C-a>', 'gg<S-v>G')
-
 -- New tab
 keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
 -- Only keep currnt tab
