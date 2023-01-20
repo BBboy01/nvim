96453d9fdcb6a943cd1136fae09b358b29e6b22d
BBBoy01 <1156678002@qq.com>
2023-01-20 21:50:29 +0800

    feat(maps ui): add more saga's keymap and custom saga ui

--- nvim/after/plugin/lspsaga.rc.lua
+++ nvim/after/plugin/lspsaga.rc.lua
@@ -12,12 +12,49 @@ saga.setup {
     respect_root = false,
     color_mode = true,
   },
+  ui = {
+    -- currently only round theme
+    theme = 'round',
+    -- this option only work in neovim 0.9
+    title = true,
+    -- border type can be single,double,rounded,solid,shadow.
+    border = 'solid',
+    winblend = 0,
+    expand = '',
+    collapse = '',
+    preview = ' ',
+    code_action = '💡',
+    diagnostic = '🐞',
+    incoming = ' ',
+    outgoing = ' ',
+    colors = {
+      --float window normal background color
+      normal_bg = '#002129',
+      --title background color
+      title_bg = '#afd700',
+      red = '#e95678',
+      magenta = '#b33076',
+      orange = '#FF8700',
+      yellow = '#f7bb3b',
+      green = '#afd700',
+      cyan = '#36d0e0',
+      blue = '#61afef',
+      purple = '#CBA6F7',
+      white = '#d1d4cf',
+      black = '#073642',
+    },
+    kind = {},
+  },
 }
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>dc', '<Cmd>Lspsaga show_line_diagnostics<cr>', opts)
 vim.keymap.set('n', '<leader>dp', '<Cmd>Lspsaga diagnostic_jump_next<cr>', opts)
 vim.keymap.set('n', '<leader>dn', '<Cmd>Lspsaga diagnostic_jump_prev<cr>', opts)
+vim.keymap.set('n', '<leader>o', '<Cmd>Lspsaga outline<cr>', opts)
+vim.keymap.set("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+vim.keymap.set("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
+vim.keymap.set({"n", "t"}, "<C-j>", "<cmd>Lspsaga term_toggle<CR>")
 vim.keymap.set('n', 'gh', '<Cmd>Lspsaga hover_doc<cr>', opts)
 vim.keymap.set('n', 'gr', '<cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<cr>', opts)
