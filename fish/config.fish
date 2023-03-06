758b9f86bc58ffda4423076172145d3b8b1fe80b
BBboy01 <1156678002@qq.com>
2023-03-06 20:03:15 +0800

    feat(PATH): add bun.sh to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -100,6 +100,7 @@ set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
 set -gx PATH ~/.cargo/bin $PATH
 set -gx PATH ~/.bun/bin $PATH
+set -gx PATH ~/.bun/bin $PATH
 
 # proxy
 function proxy
