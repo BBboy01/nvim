693fe6821d20ce154e37ce421bfe3ef839d7dd30
BBboy01 <1156678002@qq.com>
2023-03-23 13:40:37 +0800

    feat(nvim): add neovide config

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,3 +17,15 @@ end
 if is_win then
   require('windows')
 end
+
+if vim.g.neovide then
+  -- Helper function for transparency formatting
+  local alpha = function()
+    return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
+  end
+  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+  vim.g.neovide_transparency = 0.0
+  vim.g.transparency = 0.8
+  vim.g.neovide_background_color = "#002129" .. alpha()
+  vim.g.neovide_cursor_vfx_mode = "torpedo"
+end
