edc7bed5d5c985fad1b520479d8b041bfa350217
BBBoy01 <1156678002@qq.com>
2023-07-13 21:09:08 +0800

    refactor(saga): remove default configs

--- nvim/after/plugin/lspsaga.rc.lua
+++ nvim/after/plugin/lspsaga.rc.lua
@@ -2,16 +2,6 @@ local status, saga = pcall(require, 'lspsaga')
 if (not status) then return end
 
 saga.setup {
-  server_filetype_map = {},
-  symbol_in_winbar = {
-    enable = true,
-    separator = ' ',
-    hide_keyword = true,
-    show_file = true,
-    folder_level = 2,
-    respect_root = false,
-    color_mode = true,
-  },
   ui = {
     title = true,
     border = 'rounded',
@@ -52,7 +42,7 @@ saga.setup {
     tabe = "<C-c>t",
     quit = "q",
     close = "<Esc>",
-  }
+  },
 }
 
 local opts = { noremap = true, silent = true }
@@ -64,6 +54,7 @@ vim.keymap.set("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
 vim.keymap.set("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
 vim.keymap.set({ "n", "t", "i" }, "<C-j>", "<cmd>Lspsaga term_toggle<CR>")
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga goto_definition<cr>', opts)
+vim.keymap.set('n', 'gt', '<Cmd>Lspsaga goto_type_definition<cr>', opts)
 vim.keymap.set('n', 'gh', '<Cmd>Lspsaga hover_doc<cr>', opts)
 vim.keymap.set('n', 'gr', '<cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<cr>', opts)
