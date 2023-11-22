92ed619cfbaf98787e7b03ffcc36e73976c964a4
BBboy01 <1156678002@qq.com>
2023-11-22 14:03:24 +0800

    refactor: replace plugins with mini.*

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,23 +1,11 @@
 return {
-  'numToStr/Comment.nvim',
+  'echasnovski/mini.comment',
   event = 'VeryLazy',
-  opts = function()
-    return {
-      pre_hook = function(ctx)
-        local U = require('Comment.utils')
-
-        local location = nil
-        if ctx.ctype == U.ctype.block then
-          location = require('ts_context_commentstring.utils').get_cursor_location()
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-          location = require('ts_context_commentstring.utils').get_visual_start_location()
-        end
-
-        return require('ts_context_commentstring.internal').calculate_commentstring({
-          key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
-          location = location,
-        })
+  opts = {
+    options = {
+      custom_commentstring = function()
+        return require('ts_context_commentstring.internal').calculate_commentstring() or vim.bo.commentstring
       end,
-    }
-  end,
+    },
+  },
 }
