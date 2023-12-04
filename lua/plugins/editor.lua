3f6bf98f1e54d4f9c89dbec503cfd4fbad26dc72
BBBoy01 <1156678002@qq.com>
2023-12-04 13:37:30 +0800

    feat: add ufo as code fold ui

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -273,4 +273,54 @@ return {
       { '[[', desc = 'Prev Reference' },
     },
   },
+
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = {
+      'kevinhwang91/promise-async',
+      {
+        'luukvbaal/statuscol.nvim',
+        config = function()
+          local builtin = require('statuscol.builtin')
+          require('statuscol').setup({
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
+              { text = { '%s' }, click = 'v:lua.ScSa' },
+              { text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' },
+            },
+          })
+        end,
+      },
+    },
+    event = 'BufReadPost',
+    opts = {
+      provider_selector = function()
+        return { 'treesitter', 'indent' }
+      end,
+    },
+    keys = {
+      {
+        'zR',
+        function()
+          require('ufo').openAllFolds()
+        end,
+      },
+      {
+        'zM',
+        function()
+          require('ufo').closeAllFolds()
+        end,
+      },
+    },
+    init = function()
+      vim.o.foldcolumn = '1'
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+      vim.o.foldmethod = 'expr'
+      vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+      vim.o.foldenable = false
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+    end,
+  },
 }
