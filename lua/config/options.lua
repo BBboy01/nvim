05b97f73568ee2b02ba6ccddd83525af26ae7884
BBboy01 <1156678002@qq.com>
2023-12-14 13:33:38 +0800

    feat: update nvim config options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,77 +1,44 @@
 vim.cmd('autocmd!')
 
-local g = vim.g
+vim.g.mapleader = ';'
+vim.g.maplocalleader = ';'
+
 local opt = vim.opt
 
-g.mapleader = ';'
-g.maplocalleader = ';'
+opt.number = true -- Print line number
+opt.signcolumn = 'yes' -- Always show the signcolumn
+opt.completeopt = 'menu,menuone,noselect'
+opt.laststatus = 3 -- Global statusline
+opt.list = true -- Show some invisible characters (tabs...
 
-opt.encoding = 'utf-8'
-opt.fileencoding = 'utf-8'
-
-opt.number = true
-opt.signcolumn = 'yes'
-
-opt.cmdheight = 0
-opt.laststatus = 3
-opt.list = true
-
-opt.ruler = false
-opt.showtabline = 0
-opt.winwidth = 30
 opt.pumheight = 15
-opt.showcmd = false
-opt.hidden = true
+opt.pumblend = 10
 
 opt.listchars = { tab = '»·', nbsp = '+', trail = '·', extends = '→', precedes = '←' }
-opt.pumblend = 10
-opt.winblend = 10
 opt.undofile = true
 
-opt.magic = true
-opt.title = true
-opt.autoindent = true
-opt.hlsearch = true
 opt.backup = false
 opt.writebackup = false
-opt.showmode = false
-opt.expandtab = true
-opt.scrolloff = 2
-opt.sidescrolloff = 5
-opt.shell = 'fish'
+opt.shiftwidth = 2 -- Size of an indent
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.expandtab = true -- Use spaces instead of tabs
+opt.scrolloff = 2 -- Lines of context
+opt.sidescrolloff = 8 -- Columns of context
 opt.backupskip = '/tmp/*,/private/tmp/*'
-opt.inccommand = 'split'
-opt.ignorecase = true
-opt.smarttab = true
-opt.breakindent = true
-opt.shiftwidth = 2
-opt.tabstop = 2
-opt.ai = true -- Auto indent
-opt.si = true -- Smart indent
+opt.inccommand = 'nosplit' -- Preview incremental substitute
+opt.ignorecase = true -- Ignore case
+opt.smartcase = true -- Don't ignore case with capitals
+opt.smartindent = true -- Insert indents automatically
 opt.wrap = false -- No wrap lines
 opt.updatetime = 300 -- Faster completion
-opt.backspace = 'start,eol,indent'
 opt.path:append({ '**' }) -- Finding files - Search down into subfolders
 opt.wildignore:append({ '*/node_modules/*' })
+opt.wildmode = 'longest:full,full' -- Command-line completion mode
 
-vim.opt.cursorline = true
-vim.opt.termguicolors = true
-vim.opt.winblend = 0
-vim.opt.wildoptions = 'pum'
-vim.opt.pumblend = 5
-vim.opt.background = 'dark'
+opt.cursorline = true -- Enable highlighting of the current line
+opt.termguicolors = true -- True color support
 
-vim.opt.mouse = ''
-
--- Undercurl
-vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m"]])
-
--- Turn off paste mode when leaving insert
-vim.api.nvim_create_autocmd('InsertLeave', {
-  pattern = '*',
-  command = 'set nopaste',
-})
+opt.mouse = '' -- Disable mouse action
 
 opt.formatoptions:append({ 'r' })
 
