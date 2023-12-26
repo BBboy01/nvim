e208288c9018cedfac509170081b61fdf34f6892
BBboy01 <1156678002@qq.com>
2023-12-26 12:58:17 +0800

    chore: remove default config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,10 +3,6 @@ return {
     'craftzdog/solarized-osaka.nvim',
     lazy = false,
     priority = 1000,
-    ---@type Config
-    opts = {
-      transparent = true,
-    },
     config = function(_, opts)
       require('solarized-osaka').setup(opts)
       vim.cmd('colorscheme solarized-osaka')
