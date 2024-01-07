fd95cf58fb73325a8dd2e26ad44302e86f7242c7
BBboy01 <1156678002@qq.com>
2024-01-07 13:28:48 +0800

    refactor: move config to grouped config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -168,6 +168,108 @@ return {
   -- icons
   { 'nvim-tree/nvim-web-devicons', lazy = true },
 
+  -- treesitter
+  {
+    'nvim-treesitter/nvim-treesitter',
+    version = false,
+    build = ':TSUpdate',
+    event = 'BufRead',
+    init = function(plugin)
+      require('lazy.core.loader').add_to_rtp(plugin)
+      require('nvim-treesitter.query_predicates')
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    ---@type TSConfig
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+      ensure_installed = {
+        'bash',
+        'diff',
+        'html',
+        'toml',
+        'javascript',
+        'jsdoc',
+        'json',
+        'jsonc',
+        'lua',
+        'luadoc',
+        'luap',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'query',
+        'regex',
+        'tsx',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'yaml',
+        'astro',
+        'cmake',
+        'cpp',
+        'css',
+        'scss',
+        'fish',
+        'dockerfile',
+        'dot',
+        'go',
+        'rust',
+        'svelte',
+        'gitignore',
+      },
+      textobjects = {
+        move = {
+          enable = true,
+          goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer' },
+          goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer' },
+          goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer' },
+          goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer' },
+        },
+      },
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = 'mdx',
+        },
+      })
+      vim.treesitter.language.register('markdown', 'mdx')
+    end,
+  },
+
+  -- rainbow bracket
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    lazy = true,
+    event = 'BufRead',
+    config = function()
+      local rainbow_delimiters = require('rainbow-delimiters')
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [''] = rainbow_delimiters.strategy['global'],
+          vim = rainbow_delimiters.strategy['local'],
+        },
+        query = {
+          [''] = 'rainbow-delimiters',
+          lua = 'rainbow-blocks',
+        },
+        highlight = {
+          'RainbowDelimiterBlue',
+          'RainbowDelimiterYellow',
+          'RainbowDelimiterCyan',
+          'RainbowDelimiterViolet',
+          'RainbowDelimiterRed',
+          'RainbowDelimiterOrange',
+          'RainbowDelimiterGreen',
+        },
+      }
+    end,
+  },
+
   {
     'nvimdev/dashboard-nvim',
     event = 'VimEnter',
