49040944007550a3eb02efa39fc7c38865e6b97b
BBBoy01 <1156678002@qq.com>
2024-01-07 13:28:48 +0800

    refactor: move config to grouped config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -304,6 +304,7 @@ return {
     },
   },
 
+  -- fold ui
   {
     'kevinhwang91/nvim-ufo',
     dependencies = {
@@ -353,5 +354,55 @@ return {
       opt.foldlevel = 99
       opt.foldlevelstart = 99
     end,
+  },
+
+  -- nvim-tree
+  {
+    'kyazdani42/nvim-tree.lua',
+    opts = {
+      hijack_netrw = false,
+      sort = {
+        sorter = 'case_sensitive',
+      },
+      view = {
+        adaptive_size = true,
+        float = {
+          enable = true,
+          open_win_config = {
+            col = 999,
+          },
+        },
+      },
+      update_focused_file = {
+        enable = true,
+      },
+      renderer = {
+        group_empty = true,
+        indent_markers = { enable = true },
+        icons = {
+          git_placement = 'after',
+          glyphs = {
+            git = {
+              unstaged = '~',
+              staged = '✓',
+              unmerged = '',
+              renamed = '+',
+              untracked = '?',
+              deleted = '',
+              ignored = ' ',
+            },
+          },
+        },
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        debounce_delay = 50,
+        icons = { hint = '', info = '', warning = '', error = '' },
+      },
+    },
+    keys = {
+      { '<Space>e', '<Cmd>NvimTreeToggle<CR>', 'Toggle file struct tree' },
+    },
   },
 }
