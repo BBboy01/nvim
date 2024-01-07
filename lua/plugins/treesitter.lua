88370e04caaecc2bc7117d52a41f78a6d76ced78
BBboy01 <1156678002@qq.com>
2024-01-07 13:23:11 +0800

    feat: remove tsplayground

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,4 @@
 return {
-  { 'nvim-treesitter/playground', cmd = 'TSPlaygroundToggle' },
-
   {
     'nvim-treesitter/nvim-treesitter',
     version = false,
@@ -50,30 +48,6 @@ return {
         'rust',
         'svelte',
         'gitignore',
-      },
-      -- https://github.com/nvim-treesitter/playground#query-linter
-      query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = { 'BufWrite', 'CursorHold' },
-      },
-      playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = true, -- Whether the query persists across vim sessions
-        keybindings = {
-          toggle_query_editor = 'o',
-          toggle_hl_groups = 'i',
-          toggle_injected_languages = 't',
-          toggle_anonymous_nodes = 'a',
-          toggle_language_display = 'I',
-          focus_language = 'f',
-          unfocus_language = 'F',
-          update = 'R',
-          goto_node = '<CR>',
-          show_help = '?',
-        },
       },
       textobjects = {
         move = {
