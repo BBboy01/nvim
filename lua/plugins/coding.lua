e121d0afbd25ef0bc8eba8793f09b8bfb7f24f85
BBBoy01 <1156678002@qq.com>
2024-01-19 12:26:24 +0800

    refactor: mvoe none-ls config to coding

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -248,6 +248,30 @@ return {
   },
 
   {
+    'nvimtools/none-ls.nvim',
+    event = 'VeryLazy',
+    config = function()
+      local null_ls = require('null-ls')
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.dprint.with({
+            filetypes = { 'json', 'jsonc', 'markdown', 'python', 'toml' },
+          }),
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.rustfmt.with({
+            extra_args = { '--edition', '2021' },
+          }),
+          null_ls.builtins.diagnostics.eslint_d.with({
+            diagnostics_format = '[eslint] #{m}\n(#{c})',
+          }),
+          null_ls.builtins.diagnostics.fish,
+        },
+      })
+    end,
+  },
+
+  {
     'laytan/cloak.nvim',
     opts = {
       enabled = true,
