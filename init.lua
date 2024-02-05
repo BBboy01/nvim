9dc9a95f36f43b845737ef0fc6601063f3303e74
BBBoy01 <1156678002@qq.com>
2024-02-05 12:40:20 +0800

    chore: update neovide config

--- init.lua
+++ init.lua
@@ -6,13 +6,15 @@ require('config.keymaps')
 require('config.lazy')
 
 if vim.g.neovide then
-  -- Helper function for transparency formatting
-  local alpha = function()
-    return string.format('%x', math.floor(255 * (vim.g.transparency or 0.8)))
-  end
-  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-  vim.g.neovide_transparency = 0.0
-  vim.g.transparency = 0.8
-  vim.g.neovide_background_color = '#002129' .. alpha()
-  vim.g.neovide_cursor_vfx_mode = 'torpedo'
+  vim.g.neovide_transparency = 0.75
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_input_macos_alt_is_meta = true
+  vim.g.neovide_cursor_vfx_mode = 'pixiedust'
+  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
 end
