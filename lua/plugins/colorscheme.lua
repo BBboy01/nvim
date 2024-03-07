9ea6937f12face30dad3cd43f4e42ec2a5882910
BBBoy01 <1156678002@qq.com>
2024-03-07 12:41:44 +0800

    chore: fix colorscheme version because of bug behaviours

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,7 @@ return {
     'craftzdog/solarized-osaka.nvim',
     lazy = false,
     priority = 1000,
+    commit = '7d08de59374b32dd101359ccb81e9bd9de08db6a',
     config = function()
       vim.cmd('colorscheme solarized-osaka')
       vim.cmd.highlight('default link IndentLine Comment')
