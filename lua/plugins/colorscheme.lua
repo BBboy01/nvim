98b917d7acd10cb56f86cff65a6cb9c412efbf32
BBBoy01 <1156678002@qq.com>
2024-03-16 17:51:41 +0800

    chore: update fn call way

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd('colorscheme solarized-osaka')
+      vim.cmd.colorscheme('solarized-osaka')
       vim.api.nvim_set_hl(0, 'WinBar', {})
       vim.api.nvim_set_hl(0, 'WinBarNC', {})
     end,
