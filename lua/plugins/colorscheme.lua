fbab7c9ee6d753e0790c62f3b51d461d8ac14a27
BBboy01 <1156678002@qq.com>
2024-03-21 13:25:37 +0800

    chore: fix color theme config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,8 +5,6 @@ return {
     priority = 1000,
     config = function()
       vim.cmd.colorscheme('solarized-osaka')
-      vim.api.nvim_set_hl(0, 'WinBar', {})
-      vim.api.nvim_set_hl(0, 'WinBarNC', {})
     end,
   },
 }
