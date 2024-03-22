d02e2d99778d34bddf9e9d40dbc0c90d7b06ea28
BBBoy01 <1156678002@qq.com>
2024-03-22 12:41:08 +0800

    chore: remove dressing

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,22 +1,4 @@
 return {
-  -- better vim.ui
-  {
-    'stevearc/dressing.nvim',
-    lazy = true,
-    init = function()
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.select = function(...)
-        require('lazy').load({ plugins = { 'dressing.nvim' } })
-        return vim.ui.select(...)
-      end
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.input = function(...)
-        require('lazy').load({ plugins = { 'dressing.nvim' } })
-        return vim.ui.input(...)
-      end
-    end,
-  },
-
   -- tabs, which include filetype icons and close buttons.
   {
     'akinsho/bufferline.nvim',
