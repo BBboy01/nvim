83fc0a9c3340943f95e07775a2f898fae1e5f90a
BBBoy01 <1156678002@qq.com>
2024-04-10 12:39:30 +0800

    chore: remove useless keymaps

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -34,9 +34,6 @@ return {
       local mappings = {
         { opts.mappings.add, desc = 'Add surrounding', mode = { 'n', 'v' } },
         { opts.mappings.delete, desc = 'Delete surrounding' },
-        { opts.mappings.find, desc = 'Find right surrounding' },
-        { opts.mappings.find_left, desc = 'Find left surrounding' },
-        { opts.mappings.highlight, desc = 'Highlight surrounding' },
         { opts.mappings.replace, desc = 'Replace surrounding' },
         { opts.mappings.update_n_lines, desc = 'Update `MiniSurround.config.n_lines`' },
       }
