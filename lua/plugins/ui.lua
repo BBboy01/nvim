3d1bccb8d2956407996643b93e93588e651ee0df
BBboy01 <1156678002@qq.com>
2024-04-19 12:27:38 +0800

    feat: use live_grep_args in dashboard

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -411,7 +411,7 @@ return {
             end, desc = " Find file", icon = " ", key = "f" },
             { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
             { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
-            { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
+            { action = "Telescope live_grep_args", desc = " Find text", icon = " ", key = "g" },
             { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
             { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
             { action = "qa", desc = " Quit", icon = " ", key = "q" },
