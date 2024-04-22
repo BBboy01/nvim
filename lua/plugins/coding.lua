35774465796a6d3b78d717a7fe19fe4bbc2e9cb7
BBBoy01 <1156678002@qq.com>
2024-04-22 12:34:38 +0800

    refactor: use eslint

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -278,7 +278,7 @@ return {
       local null_ls = require('null-ls')
       null_ls.setup({
         sources = {
-          null_ls.builtins.diagnostics.eslint_d.with({
+          null_ls.builtins.diagnostics.eslint.with({
             diagnostics_format = '[eslint] #{m}\n(#{c})',
           }),
           null_ls.builtins.diagnostics.fish,
