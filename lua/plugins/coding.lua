9de6f4cf8cf9f20ffcefa439a056671d0d2de8b7
BBboy01 <1156678002@qq.com>
2024-05-21 12:43:48 +0800

    chore: use builitin comment string

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -74,19 +74,6 @@ return {
     },
   },
 
-  -- comments
-  {
-    'echasnovski/mini.comment',
-    event = 'VeryLazy',
-    opts = {
-      options = {
-        custom_commentstring = function()
-          return require('ts_context_commentstring.internal').calculate_commentstring() or vim.bo.commentstring
-        end,
-      },
-    },
-  },
-
   {
     'hrsh7th/nvim-cmp',
     version = false,
