97746a100bd8b9d5dbe09ab250aa7fb0680915f4
BBBoy01 <1156678002@qq.com>
2024-05-30 18:47:14 +0800

    refactor: remove redundant code

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -6,9 +6,11 @@ return {
   ---@class PluginLspOpts
   opts = {
     capabilities = {
-      foldingRange = {
-        dynamicRegistration = false,
-        lineFoldingOnly = true,
+      workspace = {
+        fileOperations = {
+          didRename = true,
+          willRename = true,
+        },
       },
     },
     ---@type vim.diagnostic.Opts
@@ -18,18 +20,14 @@ return {
       virtual_text = {
         spacing = 4,
         source = 'if_many',
-        prefix = '●',
-        -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
-        -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
-        -- prefix = "icons",
       },
       severity_sort = true,
       signs = {
         text = {
           [vim.diagnostic.severity.ERROR] = config.icons.diagnostics.Error,
           [vim.diagnostic.severity.WARN] = config.icons.diagnostics.Warn,
-          [vim.diagnostic.severity.HINT] = config.icons.diagnostics.Hint,
           [vim.diagnostic.severity.INFO] = config.icons.diagnostics.Info,
+          [vim.diagnostic.severity.HINT] = config.icons.diagnostics.Hint,
         },
       },
     },
@@ -42,58 +40,43 @@ return {
       dockerls = {},
       angularls = {
         root_dir = function(...)
-          return require('lspconfig.util').root_pattern('.git')(...)
+          return require('lspconfig.util').root_pattern('nx.json', 'angular.json')(...)
         end,
       },
       gopls = {},
-      tailwindcss = {
-        root_dir = function(...)
-          return require('lspconfig.util').root_pattern('.git')(...)
-        end,
-      },
-      tsserver = {
-        root_dir = function(...)
-          return require('lspconfig.util').root_pattern('.git')(...)
-        end,
-        single_file_support = false,
-        settings = {
-          typescript = {
-            inlayHints = {
-              includeInlayParameterNameHints = 'literal',
-              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-              includeInlayFunctionParameterTypeHints = true,
-              includeInlayVariableTypeHints = false,
-              includeInlayPropertyDeclarationTypeHints = true,
-              includeInlayFunctionLikeReturnTypeHints = true,
-              includeInlayEnumMemberValueHints = true,
-            },
-          },
-          javascript = {
-            inlayHints = {
-              includeInlayParameterNameHints = 'all',
-              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-              includeInlayFunctionParameterTypeHints = true,
-              includeInlayVariableTypeHints = true,
-              includeInlayPropertyDeclarationTypeHints = true,
-              includeInlayFunctionLikeReturnTypeHints = true,
-              includeInlayEnumMemberValueHints = true,
-            },
-          },
-        },
-      },
+      tailwindcss = {},
+      tsserver = {},
       jsonls = {
+        on_new_config = function(new_config)
+          new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+          vim.list_extend(new_config.settings.json.schemas, require('schemastore').json.schemas())
+        end,
         settings = {
           json = {
-            schemas = require('schemastore').json.schemas(),
             validate = { enable = true },
           },
         },
       },
       yamlls = {
+        on_new_config = function(new_config)
+          new_config.settings.yaml.schemas =
+            vim.tbl_deep_extend('force', new_config.settings.yaml.schemas or {}, require('schemastore').yaml.schemas())
+        end,
         settings = {
-          json = {
-            schemas = require('schemastore').yaml.schemas(),
-            validate = { enable = true },
+          redhat = { telemetry = { enabled = false } },
+          yaml = {
+            keyOrdering = false,
+            format = {
+              enable = true,
+            },
+            validate = true,
+            schemaStore = {
+              -- Must disable built-in schemaStore support to use
+              -- schemas from SchemaStore.nvim plugin
+              enable = false,
+              -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+              url = '',
+            },
           },
         },
       },
@@ -130,7 +113,6 @@ return {
           if not vim.loop.fs_stat(path .. '/lazy-lock.json') then
             return
           end
-
           client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
             runtime = {
               version = 'LuaJIT',
@@ -166,68 +148,41 @@ return {
         },
       },
     },
-    setup = {},
   },
   ---@param opts PluginLspOpts
   config = function(_, opts)
-    local register_capability = vim.lsp.handlers['client/registerCapability']
+    -- setup keymaps
+    vim.api.nvim_create_autocmd('LspAttach', {
+      callback = function(args)
+        local buffer = args.buf ---@type number
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client then
+          return require('plugins.lsp.keymaps').on_attach(client, buffer)
+        end
+      end,
+    })
 
-    ---@diagnostic disable-next-line: duplicate-set-field
-    vim.lsp.handlers['client/registerCapability'] = function(err, res, ctx)
-      local ret = register_capability(err, res, ctx)
-      ---@type lsp.Client
-      local client = vim.lsp.get_client_by_id(ctx.client_id)
-      local buffer = vim.api.nvim_get_current_buf()
-      require('plugins.lsp.keymaps').on_attach(client, buffer)
-      return ret
+    -- setup diagnostics signs
+    opts.diagnostics.virtual_text.prefix = function(diagnostic)
+      local icons = opts.diagnostics.signs.text or {}
+      return icons[diagnostic.severity]
     end
-
-    -- diagnostics signs
-    if vim.fn.has('nvim-0.10.0') == 0 then
-      for severity, icon in pairs(opts.diagnostics.signs.text) do
-        local name = vim.diagnostic.severity[severity]:lower():gsub('^%l', string.upper)
-        name = 'DiagnosticSign' .. name
-        vim.fn.sign_define(name, { text = icon, texthl = name, numhl = '' })
-      end
-    end
-
     vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
 
     local servers = opts.servers
-    local ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
-    if not ok then
-      vim.notify('Could not load nvim-cmp')
-      return
-    end
     local capabilities = vim.tbl_deep_extend(
       'force',
       {},
-      cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+      vim.lsp.protocol.make_client_capabilities(),
+      require('cmp_nvim_lsp').default_capabilities(),
       opts.capabilities
     )
 
-    local function setup(server)
-      local server_opts = vim.tbl_deep_extend('force', {
-        capabilities = vim.deepcopy(capabilities),
-      }, servers[server] or {})
-
-      if opts.setup[server] then
-        if opts.setup[server](server, server_opts) then
-          return
-        end
-      elseif opts.setup['*'] then
-        if opts.setup['*'](server, server_opts) then
-          return
-        end
-      end
-      require('lspconfig')[server].setup(server_opts)
-    end
-
+    -- setup servers
     for server, server_opts in pairs(servers) do
-      if server_opts then
-        server_opts = server_opts == true and {} or server_opts
-        setup(server)
-      end
+      require('lspconfig')[server].setup(vim.tbl_deep_extend('force', {
+        capabilities = vim.deepcopy(capabilities),
+      }, server_opts))
     end
   end,
 }
