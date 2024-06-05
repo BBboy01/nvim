39786cdf51d5b0d2dd500c364b18bdf778a06506
BBboy01 <1156678002@qq.com>
2024-06-05 10:03:13 +0800

    chore: use lazydev for neovim cmp

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,5 +1,17 @@
 return {
   {
+    'folke/lazydev.nvim',
+    ft = 'lua', -- only load on lua files
+    opts = {
+      library = {
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+
+  { 'Bilal2453/luvit-meta', lazy = true }, -- optional `vim.uv` typings
+
+  {
     'L3MON4D3/LuaSnip',
     lazy = true,
     dependencies = {
@@ -122,6 +134,10 @@ return {
           { name = 'nvim_lsp_signature_help' },
           { name = 'luasnip' },
           { name = 'path' },
+          {
+            name = 'lazydev',
+            group_index = 0, -- skip loading LuaLS completions
+          },
         }),
         formatting = {
           format = function(_, item)
