ad9c8cea285f8867eebb19cb45cd17eb7fbeb5d3
BBboy01 <1156678002@qq.com>
2024-06-07 13:45:38 +0800

    refactor: use ftplugin

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -68,16 +68,6 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
--- wrap and check for spell in text filetypes
-vim.api.nvim_create_autocmd('FileType', {
-  group = group,
-  pattern = { '*.txt', '*.tex', '*.typ', 'gitcommit', 'markdown' },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
-})
-
 -- Auto create dir when saving a file, in case some intermediate directory does not exist
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
   group = group,
