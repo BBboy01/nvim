40224fb795e0c507abcb90dd06ebf51d41558e4d
BBBoy01 <1156678002@qq.com>
2024-06-11 12:35:36 +0800

    chore: remove unnecessary plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,12 +5,5 @@ return {
     dependencies = 'nvim-treesitter/nvim-treesitter',
     config = true,
   },
-  {
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    lazy = true,
-    opts = {
-      enable_autocmd = false,
-    },
-  },
   'b0o/schemastore.nvim', -- json schemas to use with lspconfig
 }
