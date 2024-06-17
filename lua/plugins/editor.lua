f6e6d9f250b31d5a3b5bc2042ead3e7a869cdfed
BBboy01 <1156678002@qq.com>
2024-06-17 12:56:23 +0800

    chore: update config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -286,20 +286,16 @@ return {
       { 'sf', '<CMD>Oil --float<CR>', desc = 'Open File Browser with the path of the current buffer' },
     },
     opts = {
-      default_file_explorer = true,
       delete_to_trash = true,
       skip_confirm_for_simple_edits = true,
       view_options = {
         show_hidden = true,
-        natural_order = true,
         is_always_hidden = function(name, _)
           return name == '..' or name == '.git'
         end,
       },
       float = {
-        padding = 2,
         max_width = 90,
-        max_height = 0,
       },
       win_options = {
         wrap = true,
