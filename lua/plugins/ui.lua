0e54b803c426f2be0b3c1dc071ec9dd436844a96
BBBoy01 <1156678002@qq.com>
2024-06-21 20:10:39 +0800

    chore: update noice ui config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -319,6 +319,11 @@ return {
           ['cmp.entry.get_documentation'] = true,
         },
       },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+      },
       routes = {
         {
           filter = {
@@ -331,12 +336,6 @@ return {
           },
           view = 'mini',
         },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = true,
       },
     },
   },
