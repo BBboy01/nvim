adc08e9fb1391e0c63b99c582063242e83527157
BBboy01 <1156678002@qq.com>
2024-06-28 12:48:14 +0800

    fix: update neogen config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,7 @@
 return {
   {
     'danymat/neogen',
-    lazy = true,
-    dependencies = 'nvim-treesitter/nvim-treesitter',
-    config = true,
+    cmd = { 'Neogen' },
   },
   'b0o/schemastore.nvim', -- json schemas to use with lspconfig
 }
