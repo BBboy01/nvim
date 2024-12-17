3f139bdec8193619520331817624c58dfd52c013
BBBoy01 <1156678002@qq.com>
2024-12-17 14:07:53 +0800

    chore: update diagnostic jump severity

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -4,8 +4,20 @@ return {
   event = 'LspAttach',
   keys = {
     { '<leader>dc', '<Cmd>Lspsaga show_line_diagnostics<CR>', desc = 'Show current line diagnostic' },
-    { '<leader>dp', '<Cmd>Lspsaga diagnostic_jump_next<CR>', desc = 'Next diagnostic at current buffer' },
-    { '<leader>dn', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', desc = 'Prev diagnostic at current buffer' },
+    {
+      '[e',
+      function()
+        require('lspsaga.diagnostic'):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+      end,
+      desc = 'Prev error diagnostic at current buffer',
+    },
+    {
+      ']e',
+      function()
+        require('lspsaga.diagnostic'):goto_next({ severity = vim.diagnostic.severity.ERROR })
+      end,
+      desc = 'Next error diagnostic at current buffer',
+    },
     { '<leader>o', '<Cmd>Lspsaga outline<CR>', desc = 'Show outline of current buffer' },
     { 'gd', '<Cmd>Lspsaga goto_definition<CR>', desc = 'Goto cursorword definition' },
     { 'gt', '<Cmd>Lspsaga goto_type_definition<CR>', desc = 'Goto cursorword type definition' },
