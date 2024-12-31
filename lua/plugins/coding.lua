f65406061bd58cc74e6efccc9bfe7435e0e4451a
BBboy01 <1156678002@qq.com>
2024-12-31 13:49:08 +0800

    chore: update bigfile size

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -299,7 +299,7 @@ return {
             return true
           end
 
-          local max_filesize = 1024 * 1024 -- 1MB
+          local max_filesize = 100 * 1024 -- 100 KB
           local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
             return true
