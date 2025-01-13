24f1a57470aa226177cab97c3a1b7ead61512f35
BBBoy01 <1156678002@qq.com>
2025-01-13 13:42:16 +0800

    chore: bufferline show buffer icons

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -14,6 +14,7 @@ return {
     opts = {
       options = {
         mode = 'tabs',
+        show_buffer_icons = true,
         show_buffer_close_icons = false,
         show_close_icon = false,
         diagnostics = 'nvim_lsp',
