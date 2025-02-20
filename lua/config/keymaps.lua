da858baa5818b01701182183da9e7296076fb664
BBBoy01 <1156678002@qq.com>
2025-02-20 10:25:20 +0800

    chore: update keymap setting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,60 +1,68 @@
-local keymap = vim.keymap
+local map = vim.keymap.set
 
-keymap.set('n', 'x', '"_x')
+map('n', 'x', '"_x')
 
-keymap.set({ 'i', 'n' }, '<Esc>', '<Cmd>noh<CR><Esc>', { desc = 'Escape and clear hlsearch' })
-keymap.set('n', '<leader>l', '<Cmd>set hlsearch!<CR>', {
+map({ 'i', 'n' }, '<Esc>', '<Cmd>noh<CR><Esc>', { desc = 'Escape and clear hlsearch' })
+map('n', '<leader>l', '<Cmd>set hlsearch!<CR>', {
   noremap = true,
   silent = true,
   desc = 'Toggle matched highlight status',
 })
 
 -- paste but not yank
-keymap.set('x', 'p', [["_dP]])
+map('x', 'p', [["_dP]])
 
-keymap.set({ 'i', 'x', 'n', 's' }, '<C-c>', '<Esc>')
+map({ 'i', 'x', 'n', 's' }, '<C-c>', '<Esc>')
 
 -- better up/down
-keymap.set({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-keymap.set({ 'n', 'x' }, '<Down>', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-keymap.set({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-keymap.set({ 'n', 'x' }, '<Up>', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ 'n', 'x' }, '<Down>', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map({ 'n', 'x' }, '<Up>', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 -- Increment/decrement
-keymap.set('n', '+', '<C-a>')
-keymap.set('n', '-', '<C-x>')
+map('n', '+', '<C-a>')
+map('n', '-', '<C-x>')
 
 -- Delete a word backwards
-keymap.set('n', 'dw', 'vb"_d')
+map('n', 'dw', 'vb"_d')
 
 -- Split window
-keymap.set('n', 'si', '<Cmd>split<Return><C-w>w', { desc = 'Horizontally split current buffer' })
-keymap.set('n', 'sv', '<Cmd>vsplit<Return><C-w>w', { desc = 'Vertically split current buffer' })
+map('n', 'si', '<Cmd>split<Return><C-w>w', { desc = 'Horizontally split current buffer' })
+map('n', 'sv', '<Cmd>vsplit<Return><C-w>w', { desc = 'Vertically split current buffer' })
 
 -- Move window
-keymap.set('', 'sh', '<C-w>h', { desc = 'Move focus to left window', remap = true })
-keymap.set('', 'sk', '<C-w>k', { desc = 'Move focus to upper window', remap = true })
-keymap.set('', 'sj', '<C-w>j', { desc = 'Move focus to lower window', remap = true })
-keymap.set('', 'sl', '<C-w>l', { desc = 'Move focus to right window', remap = true })
+map('n', 'sh', '<C-w>h', { desc = 'Move focus to left window', remap = true })
+map('n', 'sk', '<C-w>k', { desc = 'Move focus to upper window', remap = true })
+map('n', 'sj', '<C-w>j', { desc = 'Move focus to lower window', remap = true })
+map('n', 'sl', '<C-w>l', { desc = 'Move focus to right window', remap = true })
 
 -- Resize window
-keymap.set('n', '<A-h>', '<C-w><', { desc = 'Decrease window width' })
-keymap.set('n', '<A-l>', '<C-w>>', { desc = 'Increase window width' })
-keymap.set('n', '<A-k>', '<C-w>+', { desc = 'Increase window height' })
-keymap.set('n', '<A-j>', '<C-w>-', { desc = 'Decrease window height' })
+map('n', '<A-h>', '<C-w><', { desc = 'Decrease window width' })
+map('n', '<A-l>', '<C-w>>', { desc = 'Increase window width' })
+map('n', '<A-k>', '<C-w>+', { desc = 'Increase window height' })
+map('n', '<A-j>', '<C-w>-', { desc = 'Decrease window height' })
 
 -- Save & Close buffer
-keymap.set('n', 'sw', '<Cmd>wq<CR>', { desc = 'Save and Close current buffer' })
-keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<Cmd>w<CR><Esc>', { desc = 'Save current buffer' })
+map('n', 'sw', '<Cmd>wq<CR>', { desc = 'Save and Close current buffer' })
+map({ 'i', 'x', 'n', 's' }, '<C-s>', '<Cmd>w<CR><Esc>', { desc = 'Save current buffer' })
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map('n', 'n', "'Nn'[v:searchforward].'zv'", { expr = true, desc = 'Next Search Result' })
+map('x', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
+map('o', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
+map('n', 'N', "'nN'[v:searchforward].'zv'", { expr = true, desc = 'Prev Search Result' })
+map('x', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
+map('o', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
 
 -- quit
-keymap.set('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit all' })
+map('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit all' })
 
 -- Add undo break-points
-keymap.set('i', ',', ',<c-g>u')
-keymap.set('i', '.', '.<c-g>u')
-keymap.set('i', ';', ';<c-g>u')
+map('i', ',', ',<c-g>u')
+map('i', '.', '.<c-g>u')
+map('i', ';', ';<c-g>u')
 
 -- Better indenting
-keymap.set('v', '<', '<gv')
-keymap.set('v', '>', '>gv')
+map('v', '<', '<gv')
+map('v', '>', '>gv')
