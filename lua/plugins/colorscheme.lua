622583ded10f64a741da97614fccfcc80ae6aa42
BBboy01 <1156678002@qq.com>
2025-04-09 11:40:56 +0800

    chore: make hl StatusLine transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,8 @@ return {
     priority = 1000,
     config = function()
       vim.cmd.colorscheme('solarized-osaka')
+      vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'NONE' })
+      vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'NONE' })
     end,
   },
 }
