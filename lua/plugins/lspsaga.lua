ee140f0f4516c6935e22b82b3a94adef4d03dce6
hebin <1156678002@qq.com>
2025-07-29 11:16:41 +0800

    chore: use neovim default key maps

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -20,13 +20,13 @@ return {
     },
     { '<leader>o', '<Cmd>Lspsaga outline<CR>', desc = 'Show outline of current buffer' },
     { 'gd', '<Cmd>Lspsaga goto_definition<CR>', desc = 'Goto cursorword definition' },
-    { 'gt', '<Cmd>Lspsaga goto_type_definition<CR>', desc = 'Goto cursorword type definition' },
+    { 'grt', '<Cmd>Lspsaga goto_type_definition<CR>', desc = 'Goto cursorword type definition' },
     { 'K', '<Cmd>Lspsaga hover_doc<CR>', desc = 'Show cursorword doc' },
-    { 'gr', '<Cmd>Lspsaga finder<CR>', desc = 'Show cursorword finder' },
+    { 'grr', '<Cmd>Lspsaga finder<CR>', desc = 'Show cursorword finder' },
     { 'gP', '<Cmd>Lspsaga peek_type_definition<CR>', desc = 'Peek cursorword type definition' },
     { 'gp', '<Cmd>Lspsaga peek_definition<CR>', desc = 'Peek cursorword definition' },
-    { 'ga', '<Cmd>Lspsaga code_action<CR>', desc = 'Show code action' },
-    { '<Leader>n', '<Cmd>Lspsaga rename<CR>', desc = 'Rename cursorword' },
+    { 'gra', '<Cmd>Lspsaga code_action<CR>', desc = 'Show code action' },
+    { 'grn', '<Cmd>Lspsaga rename<CR>', desc = 'Rename cursorword' },
   },
   opts = {
     outline = {
