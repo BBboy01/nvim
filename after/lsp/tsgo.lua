2471d4ceccc0027f38bfe61f5716556598e4486c
hebin <1156678002@qq.com>
2026-01-29 10:07:00 +0800

    chore: remove unknown fts

--- after/lsp/tsgo.lua
+++ after/lsp/tsgo.lua
@@ -4,10 +4,8 @@ return {
   filetypes = {
     'javascript',
     'javascriptreact',
-    'javascript.jsx',
     'typescript',
     'typescriptreact',
-    'typescript.tsx',
   },
   root_dir = function(bufnr, on_dir)
     local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lock' }
