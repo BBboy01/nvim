123c5c7a1225c23b9afc689c29e4f5c6d715af48
BBboy01 <1156678002@qq.com>
2026-05-26 16:07:14 +0800

    feat: support ssh copy

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,18 @@
 vim.g.mapleader = ';'
 vim.g.maplocalleader = ';'
 
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
 local opt = vim.opt
 
 -- Editing
